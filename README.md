bbc9ce0691e46f9cd61dea32e6a1418025292be4
Lucasmanuelmz <155545753+Lucasmanuelmz@users.noreply.github.com>
2024-02-16 20:14:04 +0200

    This was a very challenging project despite being simple afterwards.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # objet-book
+
+This is a practical project to enhance my skills in constructor objects in JavaScript. The project is guided by 'TOP' on theodinproject.com
---
+++ scripts/index.js
@@ -0,0 +1,98 @@
+let button = document.querySelector('#add-book');
+let table = document.querySelector('table');
+
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.Library = function() {
+    
+    myLibrary.push(this);
+
+    let tbody = document.querySelector('tbody');
+    let tr = document.createElement('tr')
+    tbody.appendChild(tr);
+    let tdTitle = document.createElement('td');
+    tdTitle.textContent = this.title;
+    tr.appendChild(tdTitle)
+    let tdAuthor = document.createElement('td');
+    tdAuthor.textContent = this.author;
+    tr.appendChild(tdAuthor);
+    let tdPages = document.createElement('td');
+    tdPages.textContent= this.pages
+    tr.appendChild(tdPages);
+    let tdRead = document.createElement('td');
+    tdRead.textContent = this.read;
+    tr.appendChild(tdRead);
+
+    return myLibrary;
+}
+ 
+    let thead = document.querySelector('thead');
+    let trhead = document.createElement('tr');
+    thead.appendChild(trhead);
+    let th = document.createElement('th');
+    th.classList.add('title');
+    trhead.appendChild(th);
+    th.textContent='Title of Book';
+    let thAutor = document.createElement('th');
+    thAutor.textContent = 'Author';
+    trhead.appendChild(thAutor);
+    let thPages = document.createElement('th');
+    thPages.textContent = 'Pages';
+    thPages.classList.add('pages');
+    trhead.appendChild(thPages);
+    let thRead = document.createElement('th');
+    thRead.textContent = 'Reading';
+    thRead.classList.add('check');
+    trhead.appendChild(thRead);
+
+
+function addBookToLibrary() {
+
+    let title = document.querySelector('#title').value;
+    let author = document.querySelector('#author').value;
+    let pages = document.querySelector('#pages').value;
+    let read = document.querySelector('#read').checked? 'Yes':'No';
+    let p = document.querySelector('p');
+
+    if(title !== '' && isNaN(title) 
+    && author !== '' && isNaN(author)
+    && pages !== '' && !isNaN(pages) ) {
+    const myBook = new Book(title, author, pages, read);
+    myBook.Library()
+
+    table.style.display = 'table';
+    } else { 
+        if(title === '' || !isNaN(title)) {
+            p.textContent = 'Fill in the title with text and do not use numbers!';
+            p.style.color = '#f00';
+            p.style.backgroundColor = '#ddc9c9';
+            p.style.padding = '5px';
+        } else if(author === '' || !isNaN(author)) {
+            p.textContent = 'The author field must be filled with text!';
+            p.style.color = '#f00'
+            p.style.backgroundColor = '#ddc9c9';
+            p.style.padding = '5px';
+        } else if(pages === '' || isNaN(pages)) {
+            p.textContent = 'Fill in pages with a valid number!';
+            p.style.color = '#f00';
+            p.style.backgroundColor = '#ddc9c9';
+            p.style.padding = '5px';
+        } else {
+            p.textContent = 'Serious error! Report this error.';
+            p.style.color = '#f00';
+            p.style.backgroundColor = '#ddc9c9';
+            p.style.padding = '5px';
+        }
+    }
+    
+}
+
+button.addEventListener('click', addBookToLibrary);
+
